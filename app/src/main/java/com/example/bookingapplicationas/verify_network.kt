596031cac28bab304b9c8be538867307e7dcd0d3
Gabriel Hansley Suarez <suarez.hanslee@gmail.com>
2019-10-18 17:17:25 +0800

    +ADDED layout for account settings module

---
+++ app/src/main/java/com/example/bookingapplicationas/verify_network.kt
@@ -0,0 +1,23 @@
+package com.example.bookingapplicationas
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.net.*
+
+class verify_network : AppCompatActivity() {
+
+    /*fun verifyAvailableNetwork(activity:AppCompatActivity):Boolean
+    {
+        var connectivityManager=activity.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        var networkInfo = connectivityManager.getActiveNetwork
+        return  networkInfo != null && networkInfo.equals(true)
+    }*/
+
+    /*fun netwok_isAvailable(activity:AppCompatActivity) :Boolean {
+        var connectivityManager = activity.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        var activeNetwork: NetworkInfo? = connectivityManager.getActiveNetworkInfo()
+        var isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
+
+        return isConnected
+    }*/
+}
